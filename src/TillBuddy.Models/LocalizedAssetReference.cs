b75f2929dc7597ce98bd07880eb26e8738436378
Kai-Rune <kai.rune@slettemoen.no>
2024-05-16 17:07:52 +0200

    Added LocalizedAssetReference

---
+++ src/TillBuddy.Models/LocalizedAssetReference.cs
@@ -0,0 +1,56 @@
+﻿using System.Text.Json.Serialization;
+
+namespace TillBuddy.Models;
+
+public interface ILocalizedAssetReference
+{
+    public string Group { get; set; }
+
+    /// <summary>
+    /// An asset can point to an asset or an external url
+    /// </summary>
+    [JsonPropertyName("assetId")]
+    public Guid? AssetId { get; set; }
+
+    [JsonPropertyName("text")]
+    public ILocalizedText Text { get; set; }
+
+    [JsonPropertyName("url")]
+    public ILocalizedText Url { get; set; }
+}
+
+public class LocalizedAssetReferenceRequest : ILocalizedAssetReference
+{
+    [JsonPropertyName("group")]
+    public string Group { get; set; } = string.Empty;
+
+    /// <summary>
+    /// An asset can point to an asset or an external url
+    /// </summary>
+    [JsonPropertyName("assetId")]
+    public Guid? AssetId { get; set; }
+
+    [JsonPropertyName("text")]
+    public ILocalizedText Text { get; set; } = null!;
+
+    [JsonPropertyName("url")]
+    public ILocalizedText Url { get; set; } = null!;
+}
+
+public class LocalizedAssetReferenceResponse : ILocalizedAssetReference
+{
+    [JsonPropertyName("group")]
+    public string Group { get; set; } = string.Empty;
+
+    /// <summary>
+    /// An asset can point to an asset or an external url
+    /// </summary>
+    [JsonPropertyName("assetId")]
+    public Guid? AssetId { get; set; }
+
+    [JsonPropertyName("text")]
+    public ILocalizedText Text { get; set; } = null!;
+
+    [JsonPropertyName("url")]
+    public ILocalizedText Url { get; set; } = null!;
+}
