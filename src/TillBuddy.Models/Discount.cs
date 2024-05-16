e680ec0add79083a08ccf45cd38f0c58f982e9f2
Kai-Rune <kai.rune@slettemoen.no>
2024-05-16 17:19:05 +0200

    Adding Discount

---
+++ src/TillBuddy.Models/Discount.cs
@@ -0,0 +1,28 @@
+﻿namespace TillBuddy.Models;
+
+public interface IDiscount
+{
+    public IMoney Money { get; set; }
+
+    public decimal Percent { get; set; }
+
+    public string Intention { get; set; }
+}
+
+public class DiscountRequest : IDiscount
+{
+    public IMoney Money { get; set; } = null!;
+
+    public decimal Percent { get; set; }
+
+    public string Intention { get; set; } = null!;
+}
+
+public class DiscountResponse : IDiscount
+{
+    public IMoney Money { get; set; } = null!;
+
+    public decimal Percent { get; set; }
+
+    public string Intention { get; set; } = null!;
+}
