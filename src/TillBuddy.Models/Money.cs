2bda27016ebdbe8c327439c4b12f3cebd248bb41
Kai-Rune <kai.rune@slettemoen.no>
2024-05-16 17:18:58 +0200

    Adding money

---
+++ src/TillBuddy.Models/Money.cs
@@ -0,0 +1,21 @@
+﻿namespace TillBuddy.Models;
+
+public interface IMoney
+{
+    public decimal Amount { get; set; }
+    public string Currency { get; set; }
+}
+
+public class MoneyRequest : IMoney
+{
+    public decimal Amount { get; set; }
+
+    public string Currency { get; set; } = null!;
+}
+
+public class MoneyResponse : IMoney
+{
+    public decimal Amount { get; set; }
+
+    public string Currency { get; set; } = null!;
+}
