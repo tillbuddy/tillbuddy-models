9c869099ffe8a189fb65644ff724a304ceac5eb6
Kai-Rune <kai.rune@slettemoen.no>
2024-05-16 13:56:04 +0200

    Adding address and coordinates

---
+++ src/TillBuddy.Models/Address.cs
@@ -0,0 +1,21 @@
+﻿namespace TillBuddy.Models;
+
+public class AddressRequest
+{
+    public string AddressLine { get; set; } = null!;
+    public string City { get; set; } = null!;
+    public string PostalCode { get; set; } = null!;
+    public string Country { get; set; } = null!;
+    public CoordinatesRequest? Location { get; set; }
+    public string? Coordinates { get; set; }
+}
+
+public class AddressResponse
+{
+    public string AddressLine { get; set; } = null!;
+    public string City { get; set; } = null!;
+    public string PostalCode { get; set; } = null!;
+    public string Country { get; set; } = null!;
+    public CoordinatesResponse? Location { get; set; }
+    public string? Coordinates { get; set; }
+}
---
+++ src/TillBuddy.Models/Coordinates.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TillBuddy.Models;
+
+public class CoordinatesRequest
+{
+    public double Latitude { get; set; }
+    public double Longitude { get; set; }
+}
+
+public class CoordinatesResponse
+{
+    public double Latitude { get; set; }
+    public double Longitude { get; set; }
+}
