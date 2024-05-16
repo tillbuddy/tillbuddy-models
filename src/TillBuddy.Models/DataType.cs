ac9beb0fb3e5fbfd49848b860a9ca733e135ed65
Kai-Rune <kai.rune@slettemoen.no>
2024-05-16 16:34:44 +0200

    Adding DataType

---
+++ src/TillBuddy.Models/DataType.cs
@@ -0,0 +1,17 @@
+﻿namespace TillBuddy.Models;
+
+public enum DataType
+{
+    Text,
+    LocalizedText,
+    Integer,
+    DateTime,
+    Decimal,
+    Bool,
+    List,
+    LocalizedList,
+    Asset,
+    Json,
+    Xml,
+    Url
+}
