0fc78b55a2342c98b15bdf191d6da1c0138613a0
Kai-Rune <kai.rune@slettemoen.no>
2024-05-16 15:53:29 +0200

    Adding LocalizedText

---
+++ src/TillBuddy.Models/LocalizedText.cs
@@ -0,0 +1,22 @@
+﻿namespace TillBuddy.Models;
+
+public interface ILocalizedText
+{
+    public string Text { get; set; }
+    public bool UseTranslation { get; set; }
+    public IDictionary<string, string> Translations { get; set; }
+}
+
+public class LocalizedTextRequest : ILocalizedText
+{
+    public string Text { get; set; } = null!;
+    public bool UseTranslation { get; set; }
+    public IDictionary<string, string> Translations { get; set; } = null!;
+}
+
+public class LocalizedTextResponse : ILocalizedText
+{
+    public string Text { get; set; } = null!;
+    public bool UseTranslation { get; set; }
+    public IDictionary<string, string> Translations { get; set; } = null!;
+}
