c0e5adf1cf411c2526caf441bf57f3ae4b9fdb98
Kai-Rune <kai.rune@slettemoen.no>
2024-05-16 18:38:38 +0200

    Upgrading LocalizedTextRequest with constructors

--- src/TillBuddy.Models/LocalizedText.cs
+++ src/TillBuddy.Models/LocalizedText.cs
@@ -12,6 +12,35 @@ public class LocalizedTextRequest : ILocalizedText
     public string Text { get; set; } = null!;
     public bool UseTranslation { get; set; }
     public IDictionary<string, string> Translations { get; set; } = null!;
+
+    public LocalizedTextRequest()
+    {
+        Text = string.Empty;
+        Translations = new Dictionary<string, string>(StringComparer.InvariantCultureIgnoreCase);
+    }
+
+    public static implicit operator LocalizedTextRequest(string text)
+    {        
+        return new LocalizedTextRequest { Text = text, UseTranslation = false };
+    }
+
+    public static implicit operator LocalizedTextRequest(Dictionary<string, string> translations)
+    {
+        return new (translations);
+    }
+
+    public LocalizedTextRequest(IDictionary<string, string> translations)
+    {
+        Text = string.Empty;
+        Translations = translations;
+        UseTranslation = true;
+    }
+
+    public LocalizedTextRequest(string text)
+    {
+        Text = text;
+        Translations = new Dictionary<string, string>();
+    }
 }
 
 public class LocalizedTextResponse : ILocalizedText
@@ -19,4 +48,9 @@ public class LocalizedTextResponse : ILocalizedText
     public string Text { get; set; } = null!;
     public bool UseTranslation { get; set; }
     public IDictionary<string, string> Translations { get; set; } = null!;
+
+    public static implicit operator LocalizedTextResponse(string text)
+    {
+        return new LocalizedTextResponse { Text = text, UseTranslation = false };
+    }
 }
