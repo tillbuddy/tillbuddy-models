bbaea255c767f794dacc43696494e5208f156098
Kai-Rune <kai.rune@slettemoen.no>
2024-05-16 17:22:14 +0200

    Adding JobStatus

---
+++ src/TillBuddy.Models/JobStatus.cs
@@ -0,0 +1,15 @@
+﻿namespace TillBuddy.Models;
+
+public enum JobStatus
+{
+    Pending,
+    Running,
+    Success,
+    Failed,
+    Scheduled,
+    Canceling,
+    Canceled,
+    Skipped,
+    Expired,
+    Paused
+}
