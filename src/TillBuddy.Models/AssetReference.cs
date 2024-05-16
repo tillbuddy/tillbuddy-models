0e0de88d589f667579911d926a9492a6ffa1af38
Kai-Rune <kai.rune@slettemoen.no>
2024-05-16 17:41:53 +0200

    Adding AssetReference

---
+++ src/TillBuddy.Models/AssetReference.cs
@@ -0,0 +1,55 @@
+﻿using System.Text.Json.Serialization;
+
+namespace TillBuddy.Models
+{
+    public interface IAssetReference
+    {
+        [JsonPropertyName("group")]
+        public string Group { get; set; }
+        /// <summary>
+        /// An asset can point to an asset or an external url
+        /// </summary>
+        [JsonPropertyName("assetId")]
+        public Guid? AssetId { get; set; }
+
+        [JsonPropertyName("text")]
+        public string Text { get; set; }
+
+        [JsonPropertyName("url")]
+        public string Url { get; set; }
+    }
+
+    public class AssetReferenceRequest : IAssetReference
+    {
+        [JsonPropertyName("group")]
+        public string Group { get; set; } = string.Empty;
+        /// <summary>
+        /// An asset can point to an asset or an external url
+        /// </summary>
+        [JsonPropertyName("assetId")]
+        public Guid? AssetId { get; set; }
+
+        [JsonPropertyName("text")]
+        public string Text { get; set; } = null!;
+
+        [JsonPropertyName("url")]
+        public string Url { get; set; } = null!;
+    }
+
+    public class AssetReferenceResponse : IAssetReference
+    {
+        [JsonPropertyName("group")]
+        public string Group { get; set; } = string.Empty;
+        /// <summary>
+        /// An asset can point to an asset or an external url
+        /// </summary>
+        [JsonPropertyName("assetId")]
+        public Guid? AssetId { get; set; }
+
+        [JsonPropertyName("text")]
+        public string Text { get; set; } = null!;
+
+        [JsonPropertyName("url")]
+        public string Url { get; set; } = null!;
+    }
+}
