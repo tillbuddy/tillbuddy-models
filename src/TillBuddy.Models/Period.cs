accf498572ac154ae9a0fdcfdb4264c7ee82f398
Kai-Rune <kai.rune@slettemoen.no>
2024-05-16 17:39:29 +0200

    Adding Period

---
+++ src/TillBuddy.Models/Period.cs
@@ -0,0 +1,19 @@
+﻿namespace TillBuddy.Models;
+
+public interface IPeriod
+{
+    public DateTime From { get; set; }
+    public DateTime To { get; set; }
+}
+
+public class PeriodRequest : IPeriod
+{
+    public DateTime From { get; set; }
+    public DateTime To { get; set; }   
+}
+
+public class PeriodResponse : IPeriod
+{
+    public DateTime From { get; set; }
+    public DateTime To { get; set; }
+}
