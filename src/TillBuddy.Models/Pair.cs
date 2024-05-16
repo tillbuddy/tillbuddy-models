eef7be41e1506b8c2bbf58f4eb89a9cdf5bfeadc
Kai-Rune <kai.rune@slettemoen.no>
2024-05-16 16:37:43 +0200

    Adding Pair

---
+++ src/TillBuddy.Models/Pair.cs
@@ -0,0 +1,36 @@
+﻿using System.Text.Json.Serialization;
+
+namespace TillBuddy.Models
+{
+    public interface IPair<T1, T2>
+    {
+        public T1 Id { get; set; }
+        public T2 Name { get; set; }
+    }
+
+    public interface IPair
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class Pair<T1, T2> : IPair<T1, T2>
+    {
+        [JsonPropertyName("id")]
+        public T1 Id { get; set; }
+
+        [JsonPropertyName("name")]
+        public T2 Name { get; set; }
+
+        public Pair(T1 id, T2 name)
+        {
+            Id = id;
+            Name = name;
+        }
+    }
+
+    public class Pair(string id, string name) : Pair<string, string>(id, name), IPair
+    {
+    }
+
+}
