56e7d29d68ce687752fb9ff13b8c165c61808d99
Kai-Rune <kai.rune@slettemoen.no>
2024-05-20 15:13:28 +0200

    Adding CampaignDiscount and CampaignInfo

---
+++ src/TillBuddy.Models/CampaignDiscount.cs
@@ -0,0 +1,27 @@
+﻿namespace TillBuddy.Models;
+public interface ICampaignDiscount
+{
+    public string GroupId { get; set; }
+    public IMoney? Discount { get; set; }
+    public IMoney? DiscountShare { get; set; }
+    public bool IsVisible { get; set; }
+    public ICampaignInfo? CampaignInfo { get; set; }
+}
+
+public class CampaignDiscount : ICampaignDiscount
+{
+    public string GroupId { get; set; } = null!;
+    public IMoney? Discount { get; set; }
+    public IMoney? DiscountShare { get; set; }
+    public bool IsVisible { get; set; }
+    public ICampaignInfo? CampaignInfo { get; set; }
+}
+
+public class CampaignDiscountRequest : CampaignDiscount
+{
+}
+
+
+public class CampaignDiscountResponse : CampaignDiscount
+{
+}
