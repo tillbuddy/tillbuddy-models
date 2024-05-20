7c08bbf7da3ee250c6e5dd108937366ce1cc7339
Kai-Rune <kai.rune@slettemoen.no>
2024-05-20 15:15:40 +0200

    Adding VariantProperty

---
+++ src/TillBuddy.Models/VariantProperty.cs
@@ -0,0 +1,21 @@
+﻿namespace TillBuddy.Models;
+
+public interface IVariantProperty
+{
+    public string Name { get; set; }
+    public string Value { get; set; }
+}
+
+public class VariantProperty : IVariantProperty
+{
+    public string Name { get; set; } = null!;
+    public string Value { get; set; } = null!;
+}
+
+public class VariantPropertyRequest : VariantProperty
+{
+}
+
+public class VariantPropertyResponse : VariantProperty
+{
+}
