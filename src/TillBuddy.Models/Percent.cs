204bf14d4241c454ec314dee5cc239f55d3de324
Kai-Rune <kai.rune@slettemoen.no>
2024-05-20 15:09:03 +0200

    Adding Percent

---
+++ src/TillBuddy.Models/Percent.cs
@@ -0,0 +1,72 @@
+﻿using System.Globalization;
+using System.Text.RegularExpressions;
+
+namespace TillBuddy.Models;
+
+public class Percent 
+{
+    private const string RegexPattern = "((?<number>.+)%$)";
+    private static readonly Regex Regex = new Regex(RegexPattern);
+
+    /// <summary>
+    /// The value as percentage. E.g. 25% will be represented as 0.25
+    /// </summary>
+    public decimal Percentage { get; set; }
+
+    /// <summary>
+    /// The value in percent. E.g. 25% will be represented as 25
+    /// </summary>
+    public decimal Number { get; set; }
+
+    public Percent() { }
+
+    private Percent(decimal percentage)
+    {
+        Percentage = percentage;
+        Number = percentage * 100;
+    }
+
+    public static Percent FromPercentage(decimal percentage)
+    {
+        return new Percent(percentage);
+    }
+
+    public static Percent FromNumber(decimal number)
+    {
+        return new Percent(number / 100);
+    }
+
+    public static Percent Parse(string value)
+    {
+        if (TryParse(value, out var percent))
+            return percent;
+        throw new ArgumentException($"\"{RegexPattern}\"", nameof(Percent));
+    }
+
+    public static bool TryParse(string value, out Percent percent)
+    {
+        percent = new Percent();
+
+        if (value == null) return false;
+
+        var matches = Regex.Matches(value);
+        if (matches.Count == 1)
+        {
+            var numberValue = matches[0].Groups["number"].Value;
+
+            if (decimal.TryParse(numberValue, NumberStyles.Number, CultureInfo.InvariantCulture, out var number))
+            {
+                percent = FromNumber(number);
+
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public override string ToString()
+    {
+        return $"{Number.ToString("F2", CultureInfo.InvariantCulture)}%";
+    }
+}
