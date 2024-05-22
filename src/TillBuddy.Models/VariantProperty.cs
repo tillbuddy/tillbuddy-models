e61796711d085ecbae395a4056c84f00068aa64e
Kai-Rune <kai.rune@slettemoen.no>
2024-05-22 10:24:11 +0200

    Adding clone to VariantProperty

--- src/TillBuddy.Models/VariantProperty.cs
+++ src/TillBuddy.Models/VariantProperty.cs
@@ -1,6 +1,6 @@
 ﻿namespace TillBuddy.Models;
 
-public interface IVariantProperty
+public interface IVariantProperty : ICloneable
 {
     public string Name { get; set; }
     public string Value { get; set; }
@@ -11,7 +11,15 @@ public class VariantProperty : IVariantProperty
     public string Name { get; set; } = null!;
     public string Value { get; set; } = null!;
 
-    public VariantProperty Parse(IVariantProperty source)
+    public VariantProperty() { }
+
+    public VariantProperty(string name, string value)
+    {
+        Name = name;
+        Value = value;
+    }
+
+    public static VariantProperty Parse(IVariantProperty source)
     {
         return new VariantProperty
         {
@@ -20,12 +28,62 @@ public class VariantProperty : IVariantProperty
         };
     }
 
+    public VariantPropertyResponse MapToResponse()
+    {
+        return new ()
+        {
+            Name = Name,
+            Value = Value,
+        };
+    }
+
+    public VariantPropertyRequest MapToRequest()
+    {
+        return new ()
+        {
+            Name = Name,
+            Value = Value,
+        };
+    }
+
+    public virtual object Clone()
+    {
+        return new VariantProperty()
+        {
+            Name = Name,
+            Value = Value
+        };
+    }
 }
 
 public class VariantPropertyRequest : VariantProperty
 {
+    public VariantPropertyRequest() { }
+
+    public VariantPropertyRequest(string name, string value) : base(name, value) { }
+
+    public override object Clone()
+    {
+        return new VariantPropertyRequest()
+        {
+            Name = Name,
+            Value = Value
+        };
+    }
 }
 
 public class VariantPropertyResponse : VariantProperty
 {
+    public VariantPropertyResponse() { }
+
+    public VariantPropertyResponse(string name, string value) : base(name, value) { }
+    
+    public override object Clone()
+    {
+        return new VariantPropertyResponse()
+        {
+            Name = Name,
+            Value = Value
+        };
+    }
 }
