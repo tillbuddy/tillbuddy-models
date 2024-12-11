6228c9cedd9e889c115b03c84a6f77c22b5f19c9
Kai-Rune <kai.rune@slettemoen.no>
2024-12-11 23:04:38 +0100

    Added Diller and removed WinSilver

--- src/TillBuddy.Models/ExternalProviders.cs
+++ src/TillBuddy.Models/ExternalProviders.cs
@@ -4,16 +4,16 @@ public static class ExternalProviders
 {
     public const string TillBuddy = "TillBuddy";
     public const string Omnium = "Omnium";
-    public const string WinSilver = "WinSilver";
     public const string Shopify = "Shopify";
     public const string Tripletex = "Tripletex";
+    public const string Diller = "Diller";
     public const string None = "None";
 
     public static List<string> All
     {
         get
         {
-            return [TillBuddy, Omnium, WinSilver, Shopify, Tripletex, None];
+            return [TillBuddy, Omnium, Shopify, Tripletex, Diller, None];
         }
     }
 }
