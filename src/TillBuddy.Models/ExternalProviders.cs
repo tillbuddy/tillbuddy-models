bd740ba764c37a950b8b43ed6db36fd8b0c0a2a8
Kai-Rune <kai.rune@slettemoen.no>
2024-12-18 01:30:58 +0100

    Adding Retain24

--- src/TillBuddy.Models/ExternalProviders.cs
+++ src/TillBuddy.Models/ExternalProviders.cs
@@ -7,13 +7,14 @@ public static class ExternalProviders
     public const string Shopify = "Shopify";
     public const string Tripletex = "Tripletex";
     public const string Diller = "Diller";
+    public const string Retain24 = "Retain24";
     public const string None = "None";
 
     public static List<string> All
     {
         get
         {
-            return [TillBuddy, Omnium, Shopify, Tripletex, Diller, None];
+            return [TillBuddy, Omnium, Shopify, Tripletex, Diller, Retain24, None];
         }
     }
 }
